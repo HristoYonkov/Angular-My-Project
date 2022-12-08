df6a88225da8b68430fe87d43bafb94f9fa60f70
Hristo Yonkov <hristopturs@gmail.com>
2022-12-08 20:38:00 +0200

    my books component created

--- Angular-FrontEnd/Books-Store/src/app/auth/auth-routing.module.ts
+++ Angular-FrontEnd/Books-Store/src/app/auth/auth-routing.module.ts
@@ -1,5 +1,6 @@
 import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
+import { MyBooksComponent } from "./my-books/my-books.component";
 import { RegisterComponent } from "./register/register.component";
 
 const routes: Routes = [
@@ -14,6 +15,10 @@ const routes: Routes = [
             {
                 path: 'register',
                 component: RegisterComponent
+            },
+            {
+                path: 'my-books',
+                component: MyBooksComponent
             },
         ]
     }
--- Angular-FrontEnd/Books-Store/src/app/auth/auth.module.ts
+++ Angular-FrontEnd/Books-Store/src/app/auth/auth.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { MyBooksComponent } from './my-books/my-books.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    MyBooksComponent
   ],
   imports: [
     CommonModule,
