621e6b5698220163817cdecdb67edb80112c74d8
Hristo Yonkov <hristopturs@gmail.com>
2022-12-12 17:56:25 +0200

    create template validation added

--- Angular-FrontEnd/Books-Store/src/app/books/create/create.component.ts
+++ Angular-FrontEnd/Books-Store/src/app/books/create/create.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  createHandler(form: NgForm) {
+    console.log(form.value);
+    
   }
 
 }
