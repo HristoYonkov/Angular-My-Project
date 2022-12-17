c353b4aed1563e3c00a5c042f37a9f59d31cb237
Hristo Yonkov <hristopturs@gmail.com>
2022-12-17 09:17:32 +0200

    update

--- RestService/controllers/bookController.js
+++ RestService/controllers/bookController.js
@@ -75,7 +75,11 @@ bookController.get('/:id', async (req, res) => {
         return res.status(200).json(book)
         
     } catch (error) {
+<<<<<<< Updated upstream
         return res.status(404).json(error)
+=======
+        res.status(400).json({ error: err.message })
+>>>>>>> Stashed changes
         
     }
 });
